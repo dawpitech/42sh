7be182b50b24f3c731413ff8b85cec4d17256195
Antoine <me@antoinebellanger.fr>
2024-04-28 16:01:26 +0200

    [ENHANCEMENT] Improve user prompt informations and style

---
+++ include/ansi_chars.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** 42sh
+** File description:
+** ansi_chars header
+*/
+#ifndef INC_42SH_ANSI_CHARS_H
+    #define INC_42SH_ANSI_CHARS_H
+    #define AC_RESET ("\033[0m")
+    #define AC_BOLD ("\033[1m")
+    #define AC_UNDERLINE ("\033[4m")
+    #define AC_C_RED ("\033[31m")
+    #define AC_C_GREEN ("\033[32m")
+    #define AC_C_BLUE ("\033[34m")
+    #define AC_C_BRIGHT_BLUE ("\033[96m")
+    #define AC_C_MAGENTA ("\033[95m")
+#endif //INC_42SH_ANSI_CHARS_H
